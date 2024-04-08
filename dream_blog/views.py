2df8297a2b0c6006674a8ec6d925bddb92b818fd
jukanntenn <jukanntenn@outlook.com>
2024-04-08 08:07:39 +0800

    improve pagination

--- dream_blog/views.py
+++ dream_blog/views.py
@@ -7,7 +7,20 @@ class HomeView(SetHeadlineMixin, ListView):
     template_name = "pages/home.html"
     context_object_name = "posts"
     headline = "追梦人物的博客"
-    paginate_by = 10
+    paginate_by = 20
+    paginate_orphans = 3
 
     def get_queryset(self):
         return Post.objects.visible().with_comment_count().order_by("-publish_date")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        paginator = context_data["paginator"]
+        page_obj = context_data["page_obj"]
+        elided_page_range = paginator.get_elided_page_range(
+            page_obj.number,
+            on_each_side=2,
+            on_ends=1,
+        )
+        context_data["elided_page_range"] = elided_page_range
+        return context_data
